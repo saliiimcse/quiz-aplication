c5dde22cb4f75ba46ff94d456569d73b11240c66
Salim Ahmed <selim11bd@gmail.com>
2022-12-23 16:34:11 +0600

    install tailwindcss

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav>
+            <div>
+                <h3></h3>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/components/Topics/Topics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Topics = () => {
+    return (
+        <div>
+            <h1>Topics section</h1>
+        </div>
+    );
+};
+
+export default Topics;
