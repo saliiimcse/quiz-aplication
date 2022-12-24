10df0546dc2e6293ae160c6ecb16f3da0f33f039
Salim Ahmed <selim11bd@gmail.com>
2022-12-25 01:30:43 +0600

    active route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
+    const navLInkStyle = ({isActive}) =>{
+        return {
+            color: isActive ? 'red' : 'black'
+        }
+    }
     return (
         <nav className='md:flex md:justify-between py-2 bg-red-100'>
             <div className='text-xl font-bold md:pl-10'>
                 <h3 className='sm:text-center'>Coding School</h3>
             </div>
             <div className='md:flex space-x-4 md:pr-10 font-semibold'>
-                <Link to='/'>Home</Link>
-                <Link to='/statistic'>Statistics</Link>
-                <Link to='/blog'>Blog</Link>
+                <NavLink style={navLInkStyle} to='/'>Home</NavLink>
+                <NavLink style={navLInkStyle} to='/statistic'>Statistics</NavLink>
+                <NavLink style={navLInkStyle} to='/blog'>Blog</NavLink>
             </div>
         </nav>
     );
