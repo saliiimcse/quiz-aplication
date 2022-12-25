ff5f89832abc9358f38e876f895d2077b5ad1abe
Salim Ahmed <selim11bd@gmail.com>
2022-12-25 22:14:44 +0600

    Details button color

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -9,8 +9,8 @@ const Topics = ({topicData}) => {
         <div className='border-2 border-green-400 my-3 p-12'>
             <div>
                 <h1>{name}</h1>
-                <img className='w-24 h-24' src={logo} alt="" />
-                <Link to={`./quiz/${id}`}>Details</Link>
+                <img className='w-24 h-24 m-3' src={logo} alt="" />
+                <Link to={`./quiz/${id}`} className='rounded-lg rounded-lg bg-green-300 px-6'>Details</Link>
             </div>
         </div>
     );
